a69a5121fbe4481b7256adc61cb6237a7f3dd7f6
NtouKe <255889853+NtouKe@users.noreply.github.com>
2026-01-30 18:52:03 +0200

    finished exercise

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let reverseString = "";
+  for (i = 0; i < string.length; ++i) {
+    reverseString += string.at(-i - 1);
+  }
+  return reverseString;
 };
 
 // Do not edit below this line
