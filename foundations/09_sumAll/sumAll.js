31676dde4946faff13c0f81448e21afe3b556e1d
NtouKe <255889853+NtouKe@users.noreply.github.com>
2026-01-30 21:47:03 +0200

    finished exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+  if (a < 0 || b < 0) return "ERROR";
 
+  let smallNumber = 0;
+  let bigNumber = 0;
+  if (b < a) {
+    smallNumber = b;
+    bigNumber = a;
+  } else {
+    smallNumber = a;
+    bigNumber = b;
+  }
+
+  let total = 0;
+  for (i = smallNumber; i <= bigNumber; i++) total += i;
+
+  return total;
 };
 
 // Do not edit below this line
