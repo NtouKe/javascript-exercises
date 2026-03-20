cbbcc7777251e7908be3494204cc54dc69f6212b
NtouKe <255889853+NtouKe@users.noreply.github.com>
2026-03-20 15:36:01 +0200

    finished

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const cleanString = string.replace(/[^\w]/g, "").toLowerCase();
+  const reverseString = cleanString.split("").reverse().join("");
+  return reverseString === cleanString;
 };
 
 // Do not edit below this line
