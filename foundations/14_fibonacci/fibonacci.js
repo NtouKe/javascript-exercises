d1f48dbb27f49b5776f82c0380351447f5da272c
NtouKe <255889853+NtouKe@users.noreply.github.com>
2026-03-20 15:58:40 +0200

    finished

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  if (n < 0) return "OOPS";
+  let a = 0;
+  let b = 1;
+  for (let i = 0; i < n; i++) {
+    [a, b] = [b, a + b];
+  }
+  return a;
 };
 
 // Do not edit below this line
