d231a79263168e5280648adfb437d55b5a1051db
NtouKe <255889853+NtouKe@users.noreply.github.com>
2026-03-20 18:34:00 +0200

    finished

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (person) {
+  const currentYear = new Date().getFullYear();
+  return person.reduce((oldest, currentPerson) => {
+    const age = (currentPerson.yearOfDeath || currentYear) - currentPerson.yearOfBirth;
+    const oldestAge = oldest ? (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth : -1;
+    return age > oldestAge ? currentPerson : oldest;
+  });
 };
 
 // Do not edit below this line
